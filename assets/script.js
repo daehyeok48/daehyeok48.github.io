c4777fe5608c8ba743c17421617d620b3438e950
daehyeok48 <dhkim_05@naver.com>
2026-07-11 02:17:34 -0700

    .

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,8 @@
+const btn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".mobile-menu");
 
-const btn=document.querySelector('.menu-btn');
-const menu=document.querySelector('.mobile-menu');
-if(btn){btn.addEventListener('click',()=>menu.classList.toggle('open'));}
+if (btn && menu) {
+  btn.addEventListener("click", () => {
+    menu.classList.toggle("open");
+  });
+}
